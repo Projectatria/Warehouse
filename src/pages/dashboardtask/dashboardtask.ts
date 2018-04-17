234001644d6b698ddba9fbd81992f9d000db8dd5
slametatria <aji.dwipangestu@atria.co.id>
2018-04-17 15:06:17 +0700

    dashboard po

--- src/pages/dashboardtask/dashboardtask.ts
+++ src/pages/dashboardtask/dashboardtask.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the DashboardtaskPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ApiProvider } from '../../providers/api/api';
+import moment from 'moment';
 
 @IonicPage()
 @Component({
@@ -14,11 +9,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'dashboardtask.html',
 })
 export class DashboardtaskPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public POALL = [];
+  public TOTALPOALL: any;
+  public POMONTH = [];
+  public TOTALPOMONTH: any;
+  public PODAY = [];
+  public TOTALPODAY: any;
+  public POFINISH = [];
+  public TOTALPOFINISH: any;
+  public POPREPARATION = [];
+  public TOTALPOPREPARATION: any;
+  public PORECEIVING = [];
+  public TOTALPORECEIVING: any;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public api: ApiProvider) {
+    this.doGetPO();
+    this.doGetPOMonth();
+    this.doGetPODay();
+    this.doGetPOFinish();
+    this.doGetPOPreparation();
+    this.doGetPOReceiving();
   }
 
   ionViewDidLoad() {
+  }
+  doGetPO() {
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status!='OPEN' AND status!='CLSD'" } })
+      .subscribe(val => {
+        this.POALL = val['data'];
+        this.TOTALPOALL = val['count']
+      });
+  }
+  doGetPOPreparation() {
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status='INP2'" } })
+      .subscribe(val => {
+        this.POPREPARATION = val['data'];
+        this.TOTALPOPREPARATION = val['count']
+      });
+  }
+  doGetPOReceiving() {
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status='INPG'" } })
+      .subscribe(val => {
+        this.PORECEIVING = val['data'];
+        this.TOTALPORECEIVING = val['count']
+      });
+  }
+  doGetPOMonth() {
+    let month = moment().format('MM');
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status!='OPEN' AND status!='CLSD' AND month(transfer_date)=" + month } })
+      .subscribe(val => {
+        this.POMONTH = val['data'];
+        this.TOTALPOMONTH = val['count']
+      });
+  }
+  doGetPODay() {
+    let day = moment().format('YYYY-MM-DD');
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status!='OPEN' AND status!='CLSD' AND transfer_date=" + "'" + day + "'" } })
+      .subscribe(val => {
+        this.PODAY = val['data'];
+        this.TOTALPODAY = val['count']
+      });
+  }
+  doGetPOFinish() {
+    this.api.get('table/purchasing_order', { params: { limit: 30, filter: "status='CLSD'" } })
+      .subscribe(val => {
+        this.POFINISH = val['data'];
+        this.TOTALPOFINISH = val['count']
+      });
+  }
+  doRefresh() {
+    this.doGetPO();
+    this.doGetPOMonth();
+    this.doGetPODay();
+    this.doGetPOFinish();
+    this.doGetPOPreparation();
+    this.doGetPOReceiving();
   }
   doPO() {
     document.getElementById('po').style.display = 'block';
